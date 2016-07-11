f0be1df20a09675241de85ed709d8d159b66164a
minesotha <minesotha@gmail.com>
2016-07-11 18:30:06 +0200

    poruszanie obiektow od i do kamery

--- Assets/Scripts/MoveRaycast.cs
+++ Assets/Scripts/MoveRaycast.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class MoveRaycast : MonoBehaviour
 {
 
+    float v = Input.GetAxisRaw("Vertical");
+    float h = Input.GetAxisRaw("Horizontal");
+
     public static void MoveObject(GameObject target, float moveSpeed = 10.0f)
     {
+       Vector3 camera =  Camera.main.transform.position;
 
 
         if (Input.GetKey(KeyCode.W))
-            {
-                target.transform.Translate(Vector3.forward * moveSpeed);
+        {
+            // target.transform.Translate(Vector3.forward * moveSpeed);
 
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                target.transform.Translate(Vector3.back * moveSpeed);
-            }
+             target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(-camera.x, target.transform.position.y, -camera.z), moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+              //target.transform.Translate(Vector3.back * moveSpeed);
+            target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), moveSpeed);
+
+
+
+         }
             if (Input.GetKey(KeyCode.A))
             {
                 target.transform.Translate(Vector3.left * moveSpeed);
