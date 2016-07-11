84a9cc38ec0596e3f85fbedfe1b1756173ebf900
minesotha <minesotha@gmail.com>
2016-07-11 20:19:11 +0200

    ruchy na boki wzgledem kamery

--- Assets/Scripts/MoveRaycast.cs
+++ Assets/Scripts/MoveRaycast.cs
@@ -7,33 +7,48 @@ public class MoveRaycast : MonoBehaviour
     float v = Input.GetAxisRaw("Vertical");
     float h = Input.GetAxisRaw("Horizontal");
 
-    public static void MoveObject(GameObject target, float moveSpeed = 10.0f)
-    {
-       Vector3 camera =  Camera.main.transform.position;
 
+    public static void MoveObject(GameObject target, float moveSpeed = 10.0f, float minDistanceBetween = 60.0f)
+    {
+
+        Vector3 camera = Camera.main.transform.position;
+        float distance = Vector3.Distance(new Vector3(camera.x, 0, camera.z), new Vector3(target.transform.position.x, 0, target.transform.position.z));
 
         if (Input.GetKey(KeyCode.W))
         {
-            // target.transform.Translate(Vector3.forward * moveSpeed);
-
-             target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), -moveSpeed);
+            if (distance > minDistanceBetween)
+            {
+                target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), -moveSpeed);
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
-              //target.transform.Translate(Vector3.back * moveSpeed);
-            target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), moveSpeed);
-
-
-
-         }
-            if (Input.GetKey(KeyCode.A))
+            if (distance > minDistanceBetween)
             {
-                target.transform.Translate(Vector3.left * moveSpeed);
+                target.transform.position = Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), moveSpeed);
             }
-            if (Input.GetKey(KeyCode.D))
-            {
-                target.transform.Translate(Vector3.right * moveSpeed);
-            }
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 dir;
+            dir = Camera.main.transform.right;
+            dir.y = 0;
+            dir.Normalize();
+            Vector3 ver =   target.transform.InverseTransformDirection(dir);
+            target.transform.Translate(ver * -moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 dir;
+            dir = Camera.main.transform.right;
+            dir.y = 0;
+            dir.Normalize();
+            Vector3 ver = target.transform.InverseTransformDirection(dir);
+            target.transform.Translate(ver * moveSpeed);
+        }
         target.GetComponent<Rigidbody>().MovePosition(target.transform.position);
 
 
