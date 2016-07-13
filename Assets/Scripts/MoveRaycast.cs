7180c388946522b89fae382697fe23957ca84b4a
minesotha <minesotha@gmail.com>
2016-07-13 23:16:58 +0200

    obracanie obiektow, sila dla przesuwania

--- Assets/Scripts/MoveRaycast.cs
+++ Assets/Scripts/MoveRaycast.cs
@@ -8,7 +8,7 @@ public class MoveRaycast : MonoBehaviour
     float h = Input.GetAxisRaw("Horizontal");
 
 
-    public static void MoveObject(GameObject target, float moveSpeed = 10.0f, float minDistanceBetween = 60.0f)
+    public static void MoveObject(GameObject target, float moveSpeed = 20.0f, float minDistanceBetween = 60.0f)
     {
 
         Vector3 camera = Camera.main.transform.position;
@@ -17,14 +17,26 @@ public class MoveRaycast : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             //TODO tu tez dodac sile
-            target.GetComponent<Rigidbody>().MovePosition(Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), -moveSpeed));
-       
+            //target.GetComponent<Rigidbody>().MovePosition(Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), -moveSpeed));
+            Vector3 dir;
+            dir = Camera.main.transform.up;
+            dir.y = 0;
+            dir.Normalize();
+            Vector3 ver = target.transform.InverseTransformDirection(dir);
+            target.GetComponent<Rigidbody>().AddRelativeForce(ver * moveSpeed, ForceMode.Impulse);
+
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             if (distance > minDistanceBetween)
             {
-                target.GetComponent<Rigidbody>().MovePosition(Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), moveSpeed));
+                // target.GetComponent<Rigidbody>().MovePosition(Vector3.MoveTowards(target.transform.position, new Vector3(camera.x, target.transform.position.y, camera.z), moveSpeed));
+                Vector3 dir;
+                dir = Camera.main.transform.up;
+                dir.y = 0;
+                dir.Normalize();
+                Vector3 ver = target.transform.InverseTransformDirection(dir);
+                target.GetComponent<Rigidbody>().AddRelativeForce(ver * -moveSpeed, ForceMode.VelocityChange);
             }
         }
 
@@ -37,10 +49,9 @@ public class MoveRaycast : MonoBehaviour
             dir.Normalize();
             Vector3 ver =   target.transform.InverseTransformDirection(dir);
             //target.transform.Translate(ver * -moveSpeed);
-            target.GetComponent<Rigidbody>().AddRelativeForce(ver * -moveSpeed*10,ForceMode.Acceleration);
+            target.GetComponent<Rigidbody>().AddRelativeForce(ver * -moveSpeed,ForceMode.VelocityChange);
         }
-
-        if (Input.GetKey(KeyCode.D))
+       else if (Input.GetKey(KeyCode.D))
         {
             Vector3 dir;
             dir = Camera.main.transform.right;
@@ -48,7 +59,18 @@ public class MoveRaycast : MonoBehaviour
             dir.Normalize();
             Vector3 ver = target.transform.InverseTransformDirection(dir);
             // target.transform.Translate(ver * moveSpeed);
-            target.GetComponent<Rigidbody>().AddRelativeForce(ver * moveSpeed*10, ForceMode.Acceleration);
+            target.GetComponent<Rigidbody>().AddRelativeForce(ver * moveSpeed, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+           // target.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.up * moveSpeed, ForceMode.VelocityChange);
+            target.transform.Rotate(Vector3.up * moveSpeed/5);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            // target.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.up * -moveSpeed, ForceMode.VelocityChange);
+            target.transform.Rotate(Vector3.up * -moveSpeed/5);
         }
 
     }
