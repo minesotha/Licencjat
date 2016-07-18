2ba6ce1c9000e89226f0126bd2ef21e351a235e6
minesotha <minesotha@gmail.com>
2016-07-18 19:11:34 +0200

    ustawienia swiatla, zmiana zyrandola

---
+++ Assets/Scripts/DayNightChange.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightChange : MonoBehaviour {
+    private GameObject[] day;
+    private GameObject[] night;
+    bool isDay;
+    
+    void Start()
+    {
+        day = GameObject.FindGameObjectsWithTag("daylight");
+         night = GameObject.FindGameObjectsWithTag("nightlight");
+
+        foreach (GameObject light in day)
+        {
+            light.SetActive(true);
+        }
+        foreach (GameObject light in night)
+        {
+            light.SetActive(false);
+        }
+        isDay = true;
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.N)){
+            if (isDay)
+            {
+                foreach(GameObject light in day)
+                {
+                    light.SetActive(false);
+                }
+                foreach (GameObject light in night)
+                {
+                    light.SetActive(true);
+                }
+                isDay = false;
+            }
+            else
+            {
+                foreach (GameObject light in day)
+                {
+                    light.SetActive(true);
+                }
+                foreach (GameObject light in night)
+                {
+                    light.SetActive(false);
+                }
+                isDay = true;
+            }
+
+        }
+	
+	}
+}
